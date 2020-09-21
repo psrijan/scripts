a122f2ec10d17ffafa6cb0eeaf53311b25a12a02
Srijan Pandey <p.srijan08@gmail.com>
2020-09-21 09:49:01 -0700

    Git alias add

---
+++ gitconf.sh
@@ -0,0 +1,7 @@
+git config --global alias.co checkout 
+git config --global alias.br branch
+git config --global alias.ci commit 
+git config --global alias.p push
+git config --global alias.st status
+git config --global alias.a add
+git config --global alias.rb rebase 
